793d49aae57dc7593c8cbf3dc7c4f01ddddfc410
Raushan  Raj <raushan@Raushans-MacBook-Air.local>
2023-06-07 13:04:52 +0530

    Custom HTTP Dispatcher for

--- kube_hunter/modules/report/dispatchers.py
+++ kube_hunter/modules/report/dispatchers.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import requests
+import uuid
 
 logger = logging.getLogger(__name__)
 
@@ -8,13 +9,13 @@ logger = logging.getLogger(__name__)
 class HTTPDispatcher:
     def dispatch(self, report):
         logger.debug("Dispatching report via HTTP")
-        dispatch_method = os.environ.get("KUBEHUNTER_HTTP_DISPATCH_METHOD", "POST").upper()
-        dispatch_url = os.environ.get("KUBEHUNTER_HTTP_DISPATCH_URL", "https://localhost/")
-        headers = {"Content-Type": "application/json", "Authorization": os.environ.get("HTTP_AUTH_TOKEN","")}
+        dispatch_method = os.environ.get("KSEC_HTTP_DISPATCH_METHOD", "POST").upper()
+        dispatch_url = os.environ.get("KSEC_HTTP_DISPATCH_URL", "https://localhost/")
+        scan_id = os.environ.get("KSEC_PENTEST_SCAN_ID")
+        headers = {"Content-Type": "application/json", "Authorization": os.environ.get("HTTP_AUTH_TOKEN", "")}
+        data = {"scan_id": scan_id, "output": report, "identifier": uuid.uuid4().hex}
         try:
-            r = requests.request(
-                dispatch_method, dispatch_url, json=report, headers=headers, verify=False
-            )
+            r = requests.post(dispatch_url, data=data, headers=headers, verify=False)
             r.raise_for_status()
             logger.info(f"Report was dispatched to: {dispatch_url}")
             logger.debug(f"Dispatch responded {r.status_code} with: {r.text}")
